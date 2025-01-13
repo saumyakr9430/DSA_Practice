fc2fecb74f3656f66b483c1d13489d0ea61b38b3
saumyakr9430 <saumyakr9430@gmail.com>
2025-01-13 11:41:26 +0530

    Tender_carpenter

--- A_Tender_Carpenter.cpp
+++ A_Tender_Carpenter.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main() {
 
     int t = 0 ;
@@ -8,6 +9,28 @@ int main() {
     while(t--){
         int n ;
         cin >> n ;
+        vector<int> arr(n);
+     
+        for(int i =0; i<n ; i++){
+            
+            cin >> arr[i] ;
+        }
+
+        int ans =0 ;
+        
+        for(int i = 0 ; i<n-1 ; i++){
+            if(2*min(arr[i] ,arr[i+1]) > max(arr[i],arr[i+1])){
+                ans++;
+            }
+        }
+
+       if(ans > 0){
+        cout << "YES" << " ";
+        }
+        else{
+            cout << "NO" << " ";
+        }
+      
 
 
 
