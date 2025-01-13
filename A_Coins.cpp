5d7e9848a067981679eb3cddb520e19a11135b65
saumyakr9430 <saumyakr9430@gmail.com>
2025-01-13 18:24:09 +0530

    A_coins

---
+++ A_Coins.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t = 0 ;
+    cin >> t ;
+    while(t--){
+        long long n ;
+        cin >> n ;
+        long long k ;
+        cin >> k ;
+        
+        if(n%2 == 0){
+            cout << "YES" << endl;
+        }
+        else{
+
+            if(k%2 == 0){
+                cout << "NO" << endl ;
+            }
+            else{
+                cout << "YES" << endl ;
+            }
+        }
+
+
+
+    }
+} 
